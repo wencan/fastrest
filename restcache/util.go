6d2e11734e6bb43ca99d10497d1367817784b99b
wencan <i@wencan.org>
2022-10-17 09:24:38 +0800

    fix getTTL

--- restcache/util.go
+++ restcache/util.go
@@ -5,10 +5,9 @@ import (
 	"time"
 )
 
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
 // getTTL 计算出区间内的一个TTL值。
 func getTTL(TTLRange [2]time.Duration) time.Duration {
+	var r = rand.New(rand.NewSource(time.Now().UnixNano())) // rand不是并发安全的
 	gap := TTLRange[1] - TTLRange[0]
 	ttl := TTLRange[0] + time.Duration(r.Intn(int(gap.Seconds())))*time.Second
 
