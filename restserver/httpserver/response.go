ba0badd928230bab065b466a8edf1a5e61288370
wencan <i@wencan.org>
2022-10-18 10:14:11 +0800

    update httpserver

--- restserver/httpserver/response.go
+++ restserver/httpserver/response.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/wencan/fastrest/restcodecs/restjson"
+	"github.com/wencan/fastrest/restcodecs/restmime"
 )
 
 // WriteResponse 将响应写出。
 // 暂时只会输出为json。
 func WriteResponse(ctx context.Context, accept string, response interface{}, w http.ResponseWriter) error {
-	encoder := restjson.NewEncoder(w)
-	return encoder.Encode(response)
+	return restmime.Marshal(response, accept, w)
 }
