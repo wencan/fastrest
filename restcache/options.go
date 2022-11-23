763c30c65418ecb8e992e1984d2e81e5a086df17
wencan <i@wencan.org>
2022-11-23 23:03:18 +0800

    fix comment

--- restcache/options.go
+++ restcache/options.go
@@ -2,7 +2,7 @@ package restcache
 
 // Validatable 缓存有效性检查接口。缓存对象可选实现。
 // 警告：注意实现中ValidCache方法的接收者，一般应该是结构体对象，而不是结构体指针。
-// 时效的缓存对象可能会影响结果，可选实现Resetable接口。
+// 失效的缓存对象可能会影响结果，可选实现Resetable接口。
 type Validatable interface {
 	// IsValidCache 判断缓存对象是否还有效。返回true表示有效。
 	// 如果无效，等同没找到缓存存储数据。
