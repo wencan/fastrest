368d64fd763bb4ff588054a626b0c30a37b39a87
wencan <wencan@live.cn>
2022-12-28 20:49:09 +0800

    http client添加UserAgent

--- restclient/httpclient/client.go
+++ restclient/httpclient/client.go
@@ -13,6 +13,7 @@ var DefaultClient = Client{
 	DefaultAccept:    "*/*",
 	DoFunc:           http.DefaultClient.Do,
 	ReadResponseFunc: ReadResponse,
+	UserAgent:        "fastrest-http-client/1.1",
 }
 
 // Client 带配置的客户端。
@@ -28,10 +29,14 @@ type Client struct {
 
 	// ReadResponseFunc 解析响应的函数。默认为：ReadResponseBody
 	ReadResponseFunc ReadResponseFunc
+
+	// UserAgent 请求Header的User-Agent值。默认fastrest-http-client/1.1。
+	UserAgent string
 }
 
 // Do 发送请求，解析响应到对象。
 func (client Client) Do(ctx context.Context, dest interface{}, r *http.Request) error {
+	r.Header.Set("User-Agent", client.UserAgent)
 	if client.DefaultAccept != "" && r.Header.Get("Accept") == "" {
 		r.Header.Set("Accept", client.DefaultAccept)
 	}
