7eb9eef8f36020ce8bef21a01ff0bea7f464fe85
wencan <wencan@live.cn>
2023-01-11 16:16:48 +0800

    fix ValidateStruct

--- restutils/validator.go
+++ restutils/validator.go
@@ -2,6 +2,7 @@ package restutils
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -11,5 +12,19 @@ var validate = validator.New()
 // ValidateStruct 校验结构体对象字段值。
 // 依赖于字段validate标签。校验支持：https://github.com/go-playground/validator。
 func ValidateStruct(ctx context.Context, s interface{}) error {
-	return validate.StructCtx(ctx, s)
+	value := reflect.ValueOf(s)
+	switch value.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < value.Len(); i++ {
+			elem := value.Index(i)
+			err := ValidateStruct(ctx, elem.Interface())
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return validate.StructCtx(ctx, s)
+	}
+
+	return nil
 }
---
+++ restutils/validator_test.go
@@ -0,0 +1,190 @@
+package restutils
+
+import (
+	"context"
+	"testing"
+)
+
+func TestValidateStruct(t *testing.T) {
+	type Request struct {
+		// Page 页码。从第一页开始。
+		Page int `validate:"required,gte=1"`
+
+		// Limit 每页数量上限。
+		Limit int `validate:"required"`
+	}
+
+	type ComplexRequest struct {
+		Request Request `validate:"required"`
+		Name    string  `validate:"required"`
+	}
+
+	type ComplexSlice struct {
+		Requests []*Request `validate:"dive,required"`
+		Name     string     `validate:"required"`
+	}
+
+	tests := []struct {
+		name    string
+		args    interface{}
+		wantErr bool
+	}{
+		{
+			name: "struct_ok",
+			args: Request{
+				Page:  1,
+				Limit: 10,
+			},
+		},
+		{
+			name: "struct_fail",
+			args: Request{
+				Page:  0,
+				Limit: 10,
+			},
+			wantErr: true,
+		},
+		{
+			name: "struct_ptr_ok",
+			args: &Request{
+				Page:  1,
+				Limit: 10,
+			},
+		},
+		{
+			name: "struct_ptr_fail",
+			args: &Request{
+				Page:  0,
+				Limit: 10,
+			},
+			wantErr: true,
+		},
+		{
+			name: "struct_slice_ok",
+			args: []Request{
+				{
+					Page:  1,
+					Limit: 10,
+				}},
+		},
+		{
+			name: "struct_slice_fail",
+			args: []Request{
+				{
+					Page:  0,
+					Limit: 10,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "struct_ptr_slice_ok",
+			args: []*Request{
+				{
+					Page:  1,
+					Limit: 10,
+				}},
+		},
+		{
+			name: "struct_ptr_slice_fail",
+			args: []*Request{
+				{
+					Page:  0,
+					Limit: 10,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "complex_ok",
+			args: ComplexRequest{
+				Request: Request{
+					Page:  1,
+					Limit: 10,
+				},
+				Name: "测试",
+			},
+			wantErr: false,
+		},
+		{
+			name: "complex_fail",
+			args: ComplexRequest{
+				Request: Request{
+					Page:  0,
+					Limit: 10,
+				},
+				Name: "测试",
+			},
+			wantErr: true,
+		},
+		{
+			name: "complex_fail_2",
+			args: ComplexRequest{
+				Request: Request{
+					Page:  1,
+					Limit: 10,
+				},
+				Name: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "complex_slice_ok",
+			args: ComplexSlice{
+				Requests: []*Request{
+					{
+						Page:  1,
+						Limit: 10,
+					},
+					{
+						Page:  2,
+						Limit: 10,
+					},
+				},
+				Name: "测试",
+			},
+			wantErr: false,
+		},
+		{
+			name: "complex_slice_fail",
+			args: ComplexSlice{
+				Requests: []*Request{
+					{
+						Page:  1,
+						Limit: 10,
+					},
+					{
+						Page:  0,
+						Limit: 10,
+					},
+				},
+				Name: "测试",
+			},
+			wantErr: true,
+		},
+		{
+			name: "complex_slice_fail_2",
+			args: ComplexSlice{
+				Requests: []*Request{
+					{
+						Page:  1,
+						Limit: 10,
+					},
+					{
+						Page:  2,
+						Limit: 10,
+					},
+				},
+				Name: "",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ValidateStruct(context.TODO(), tt.args); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateStruct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
