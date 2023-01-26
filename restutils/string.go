8ad564a74f2cf7a7f79886d8b0e00fa52cb6a7b2
wencan <i@wencan.org>
2023-01-26 17:22:26 +0800

    add SubstringsWithTags

--- restutils/string.go
+++ restutils/string.go
@@ -34,3 +34,29 @@ func StringFromURL(ctx context.Context, rawUrl string) (string, error) {
 	}
 	return string(bytes), nil
 }
+
+// SubstringsWithTags 根据前后标签，找出子字符串。
+// 比如字符串为“我是一名{[(程序员)]}”，前后标签为“{[(”和“)]}”，匹配的字符串为“程序员”。
+// 返回元素顺序同它们在字符串内的顺序。
+// 如果存在重复的匹配子字符串，函数返回元素也将重复。
+func SubstringsWithTags(str, startTag, endTag string) []string {
+	var substrs []string
+	parts := strings.Split(str, startTag)
+	for idx, part := range parts {
+		if idx == 0 { // 第一个应该是空字符串
+			continue
+		}
+		if part == "" {
+			continue
+		}
+		// if !strings.Contains(part, endTag) {
+		// 	continue
+		// }
+		subs := strings.Split(part, endTag)
+		if len(subs) <= 1 {
+			continue
+		}
+		substrs = append(substrs, subs[0])
+	}
+	return substrs
+}
---
+++ restutils/string_test.go
@@ -0,0 +1,141 @@
+package restutils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSubstringsWithTags(t *testing.T) {
+	type args struct {
+		str      string
+		startTag string
+		endTag   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "empty",
+			args: args{
+				str:      "",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: nil,
+		},
+		{
+			name: "one",
+			args: args{
+				str:      "测试(这个)一下",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"这个"},
+		},
+		{
+			name: "two",
+			args: args{
+				str:      "测试(这个)一下，还有(this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"这个", "this"},
+		},
+		{
+			name: "three",
+			args: args{
+				str:      "(now)测试(这个)一下，还有(this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"now", "这个", "this"},
+		},
+		{
+			name: "strTag",
+			args: args{
+				str:      "测试{[(这个)]}一下",
+				startTag: "{[(",
+				endTag:   ")]}",
+			},
+			want: []string{"这个"},
+		},
+		{
+			name: "repeat",
+			args: args{
+				str:      "测试(this)一下，还有(this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"this", "this"},
+		},
+		{
+			name: "one_lost-endTag",
+			args: args{
+				str:      "测试(这个一下",
+				startTag: "(",
+				endTag:   ")",
+			},
+		},
+		{
+			name: "one_lost-startTag",
+			args: args{
+				str:      "测试这个)一下",
+				startTag: "(",
+				endTag:   ")",
+			},
+		},
+		{
+			name: "two_lost-endTag",
+			args: args{
+				str:      "测试(这个一下，还有(this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"this"},
+		},
+		{
+			name: "two_lost-endTag-2",
+			args: args{
+				str:      "测试(这个)一下，还有(this",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"这个"},
+		},
+		{
+			name: "two_lost_startTag",
+			args: args{
+				str:      "测试这个)一下，还有(this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"this"},
+		},
+		{
+			name: "two_lost_startTag-2",
+			args: args{
+				str:      "测试(这个)一下，还有this)",
+				startTag: "(",
+				endTag:   ")",
+			},
+			want: []string{"这个"},
+		},
+		{
+			name: "no_tag",
+			args: args{
+				str:      "测试这个一下",
+				startTag: "(",
+				endTag:   ")",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SubstringsWithTags(tt.args.str, tt.args.startTag, tt.args.endTag); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SubstringsWithTags() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
